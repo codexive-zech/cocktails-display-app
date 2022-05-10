7ff554408fe24111bb11a625ac8fec459daa60bd
Codexive_zech <zechariahhounwanou@gmail.com>
2022-05-10 16:48:19 +0100

    implemented the display drink func and a new drink func
    selected the elements that will take in effect, check if drinks are not returned from server display error msg then if drinks are avaliable then iterate over all then destructure the needed properties and add to HTML

--- src/displayDrink.js
+++ src/displayDrink.js
@@ -1,1 +1,41 @@
+import getElement from './getElement.js';
 
+//a display drink func to show drinks dynamically on the web page the { drink } is there so as to access the data in the server
+const displayDrinks = ({ drinks }) => {
+  // selected classes that will take in action and effect
+  const sectionCenter = getElement('.section-center');
+  const title = getElement('.title');
+
+  // checking to see if the drink from the server does exist
+  if (!drinks) {
+    //  TODO!!!!! Hide Loading
+    // displaying an error message when it does not exist
+    title.textContent = `Sorry, Cocktail does not exist`;
+    // do not want to display rest drinks and error msg together
+    sectionCenter.innerHTML = null;
+    return;
+  }
+  // function expression for getting new drink from the server via it stated url
+  const newDrink = drinks
+    // iterate over the drinks object in the server
+    .map((drink) => {
+      // destructured the need propertied from the object so as to use it here
+      const { idDrink: id, strDrink: name, strDrinkThumb: image } = drink;
+      // dynamically display the each drinks from the server via the url and added the destructed value where needed
+      return `<a href="drink.html">
+          <article class="cocktail" data-id="${id}">
+            <img src="${image}" alt="${name}" />
+            <h3>${name}</h3>
+          </article>
+        </a>`;
+    })
+    .join('');
+  // make the error message section be an empty string (blank)
+  title.textContent = '';
+  // dynamically insert the new drink func into the section center for the HTML
+  sectionCenter.innerHTML = newDrink;
+  // return back the section center
+  return sectionCenter;
+};
+
+export default displayDrinks;
--- src/presentDrink.js
+++ src/presentDrink.js
@@ -1,12 +1,15 @@
 // importing the fetch drinks file into present drink so it can be used
 import fetchDrinks from './fetchDrink.js';
+import displayDrinks from './displayDrink.js';
 
 // a func that present drinks takes two func fetch drinks and display drink
 const presentDrinks = async (url) => {
   // Fetch the drinks from the server
   const data = await fetchDrinks(url);
-  console.log(data);
+  // console.log(data);
   // display the drinks available in the server via the url
+  const section = await displayDrinks(data);
+  console.log(section);
 };
 
 export default presentDrinks;
